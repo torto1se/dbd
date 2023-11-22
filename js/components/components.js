5cfa37f2a7221396c7b6d037ea0112bc96d71d59
torto1se <144354809+torto1se@users.noreply.github.com>
2023-11-22 14:07:58 +0300

    Update components.js

--- js/components/components.js
+++ js/components/components.js
@@ -1,1 +1,16 @@
+Vue.component('aboutUs', {
+  template:`
+    <div align="center">
+      <h2> О нас:</h2>
+      <aboutUs__text></aboutUs__text>
+    </div>`
+});
 
+Vue.component('aboutUs__text',{
+
+});
+
+new Vue({
+  el:'#component',
+  template: '<aboutUs><aboutUs>'
+});
