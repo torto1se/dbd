9e3888180f33a9f38561c7eaff6fae40b1c11baf
torto1se <akvvsjcvdhi@gmail.com>
2023-11-22 19:25:31 +0300

    components

--- js/components/components.js
+++ js/components/components.js
@@ -1,19 +1,21 @@
 Vue.component('aboutUs', {
   template:`
-    <div align="center">
-      <h2> Об игре:</h2>
+    <div class="main__center main__center_settings">
+      <h2> 
+        Об игре:
+      </h2>
       <aboutUs__text></aboutUs__text>
     </div>`
 });
 
 Vue.component('aboutUs__text',{
-   <div class="main__center main__center_settings">
-            <h3 class="main__h3"> Dead by Daylight — асимметричная многопользовательская<br> компьютерная игра в жанре survival horror с видом от первого и третьего лица,<br> разработанная канадской студией Behaviour Interactive и впервые выпущенная для Windows в 2016 году. Кнопка была нажата {{ counter }} раз</h3>
-            <button v-on:click="clickers" @focus="handleFocus">+1</button>
-        </div>
+  template:`
+    <h3 class="main__h3" style="padding-top:30px;"> 
+      Dead by Daylight — асимметричная многопользовательская<br> компьютерная игра в жанре survival horror с видом от первого и третьего лица,<br> разработанная канадской студией Behaviour Interactive и впервые выпущенная для Windows в 2016 году.
+    </h3>`
 });
 
 new Vue({
   el:'#component',
-  template: '<aboutUs><aboutUs>'
+  template: `<aboutUs></aboutUs>`
 });
